029926c74e1216781fcb34da1b3c13cb70860a5b
ramiiiiii <ramibelkaddas@gmail.com>
2023-10-30 14:42:36 +0100

    minor changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,38 +6,36 @@ import CV from "../../assets/cv.pdf";
 
 const About = () => {
   return (
-    <div>
-      <div className="a">
-        <div className="a-left">
-          <div className="a-card bg"></div>
-          <div className="a-card">
-            <img src={skill} alt="" className="a-img" />
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img src={skill} alt="" className="a-img" />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">A dedicated Full-stack Developer.</p>
+        <p className="a-desc">
+          I am a 22 year old developer, born on March 11, 2001, passionate and
+          creative, with a solid technical expertise, a proven ability to solve
+          complex problems. I use ReactJS for frontend developement, and Laravel
+          for the backend.
+        </p>
+        <div className="a-award">
+          <img src={Master} alt="" className="a-award-img" />
+          <div className="a-award-texts">
+            <h4 className="a-award-title">
+              Master's degree in computer science
+            </h4>
+            <p className="a-award-desc">
+              Specializing in Information Systems and Web Technologies.
+            </p>
           </div>
         </div>
-        <div className="a-right">
-          <h1 className="a-title">About Me</h1>
-          <p className="a-sub">A dedicated Full-stack Developer.</p>
-          <p className="a-desc">
-            I am a 22 year old developer, born on March 11, 2001, passionate and
-            creative, with a solid technical expertise, a proven ability to
-            solve complex problems. I use ReactJS for frontend developement, and
-            Laravel for the backend.
-          </p>
-          <div className="a-award">
-            <img src={Master} alt="" className="a-award-img" />
-            <div className="a-award-texts">
-              <h4 className="a-award-title">
-                Master's degree in computer science
-              </h4>
-              <p className="a-award-desc">
-                Specializing in Information Systems and Web Technologies.
-              </p>
-            </div>
-          </div>
-          <a href={CV} download className="i-download-link">
-            Download CV
-          </a>
-        </div>
+        <a href={CV} download className="i-download-link">
+          Download CV
+        </a>
       </div>
     </div>
   );
